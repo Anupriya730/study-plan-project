fcaf6e37a2b1ad5128970dc01bb1b9d870d90756
Anupriya Palanisamy <33505857+Anupriya730@users.noreply.github.com>
2024-09-30 14:26:43 -0400

    Add files via upload

---
+++ models/questionnaire.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const questionnaireSchema = new mongoose.Schema({
+    primary_goal: {
+        type: String,
+        required: true
+    },
+    subjects_of_interest: {
+        type: Array,
+        required: true
+    },
+    confidence_level: {
+        type: String,
+        required: true
+    },
+    learning_style: {
+        type: Array,
+        required: true
+    },
+    time_dedication: {
+        type: String,
+        required: true
+    },
+    has_deadlines: {
+        type: Boolean,
+        required: true
+    },
+    has_prior_experience: {
+        type: Boolean,
+        required: true
+    },
+    study_materials: {
+        type: Array,
+        required: true
+    },
+    anticipated_challenges: {
+        type: Array,
+        required: true
+    },
+    progress_tracking: {
+        type: Array,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, { collection: 'questionnaire' });
+
+
+const Questionnaire = mongoose.model('Questionnaire', questionnaireSchema);
+
+module.exports = Questionnaire;
